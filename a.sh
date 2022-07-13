478024437848e52fa0d9f35d4ba00ebb60720aa7
Amazing-12138cat <43748486+Amazing-12138cat@users.noreply.github.com>
2022-07-13 14:17:18 +0800

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cat ./ip.txt | while read ip
+do
+        cutycapt --insecure --url=https://$ip --max-wait=5000 --out=$ip.png
+        if test ! -f $ip.png; 
+        then
+                echo $ip
+        else
+                echo $ip >>./error.txt
+        fi
+don
